8b0b5d10255aa2888fb4ea4f2fc26e580aea7209
Steve Bjorg <steve.bjorg@gmail.com>
2020-04-15 09:47:04 -0700

    added System.Text.Json example

--- Program.cs
+++ Program.cs
@@ -14,25 +14,29 @@ namespace SerializerFun {
         //--- Class Methods ---
         public static void Main(string[] args) {
 
-            // create an instance to serialize
-            var foo = new Record {
-                Foo = "Hello world!"
-            };
+        // create an instance to serialize
+        var record = new Record {
+            Foo = "Hello world!"
+        };
 
-            // show serialization with original Lambda serializer based on Newtonsoft.Json
-            var oldSerializer = SerializeWith(foo, new Amazon.Lambda.Serialization.Json.JsonSerializer());
-            Console.WriteLine($"Amazon.Lambda.Serialization.Json.JsonSerializer: {oldSerializer}");
+        // show serialization with original Lambda serializer based on Newtonsoft.Json
+        var oldSerializer = SerializeWith(record, new Amazon.Lambda.Serialization.Json.JsonSerializer());
+        Console.WriteLine($"Amazon.Lambda.Serialization.Json.JsonSerializer: {oldSerializer}");
 
-            // show serialization with new Lambda serializer based on System.Text.Json
-            var newSerializer = SerializeWith(foo, new Amazon.Lambda.Serialization.SystemTextJson.LambdaJsonSerializer());
-            Console.WriteLine($"Amazon.Lambda.Serialization.SystemTextJson.LambdaJsonSerializer: {newSerializer}");
+        // show serialization with new Lambda serializer based on System.Text.Json
+        var newSerializer = SerializeWith(record, new Amazon.Lambda.Serialization.SystemTextJson.LambdaJsonSerializer());
+        Console.WriteLine($"Amazon.Lambda.Serialization.SystemTextJson.LambdaJsonSerializer: {newSerializer}");
 
-            // local functions
-            string SerializeWith<T>(T value, Amazon.Lambda.Core.ILambdaSerializer serializer) {
-                using var buffer = new MemoryStream();
-                serializer.Serialize<T>(value, buffer);;
-                return System.Text.Encoding.UTF8.GetString(buffer.ToArray());
-            }
+        // show serialization with System.Json.Text
+        var jsonTextSerializer = System.Text.Json.JsonSerializer.Serialize<Record>(record);
+        Console.WriteLine($"System.Text.Json.JsonSerializer: {jsonTextSerializer}");
+
+        // local functions
+        string SerializeWith<T>(T value, Amazon.Lambda.Core.ILambdaSerializer serializer) {
+            using var buffer = new MemoryStream();
+            serializer.Serialize<T>(value, buffer);;
+            return System.Text.Encoding.UTF8.GetString(buffer.ToArray());
+        }
         }
     }
 }
--- ReadMe.md
+++ ReadMe.md
@@ -6,27 +6,28 @@ Support for .NET Core 3.1 in AWS Lambda also introduced support for a new serial
 ## Sample Code
 Consider the following code, which prints the serialization result using the new and old serializers.
 ```csharp
-public static void Main(string[] args) {
+// create an instance to serialize
+var record = new Record {
+    Foo = "Hello world!"
+};
 
-    // create an instance to serialize
-    var foo = new Record {
-        Foo = "Hello world!"
-    };
+// show serialization with original Lambda serializer based on Newtonsoft.Json
+var oldSerializer = SerializeWith(record, new Amazon.Lambda.Serialization.Json.JsonSerializer());
+Console.WriteLine($"Amazon.Lambda.Serialization.Json.JsonSerializer: {oldSerializer}");
 
-    // show serialization with original Lambda serializer based on Newtonsoft.Json
-    var oldSerializer = SerializeWith(foo, new Amazon.Lambda.Serialization.Json.JsonSerializer());
-    Console.WriteLine($"Amazon.Lambda.Serialization.Json.JsonSerializer: {oldSerializer}");
+// show serialization with new Lambda serializer based on System.Text.Json
+var newSerializer = SerializeWith(record, new Amazon.Lambda.Serialization.SystemTextJson.LambdaJsonSerializer());
+Console.WriteLine($"Amazon.Lambda.Serialization.SystemTextJson.LambdaJsonSerializer: {newSerializer}");
 
-    // show serialization with new Lambda serializer based on System.Text.Json
-    var newSerializer = SerializeWith(foo, new Amazon.Lambda.Serialization.SystemTextJson.LambdaJsonSerializer());
-    Console.WriteLine($"Amazon.Lambda.Serialization.SystemTextJson.LambdaJsonSerializer: {newSerializer}");
+// show serialization with System.Json.Text
+var jsonTextSerializer = System.Text.Json.JsonSerializer.Serialize<Record>(record);
+Console.WriteLine($"System.Text.Json.JsonSerializer: {jsonTextSerializer}");
 
-    // local functions
-    string SerializeWith<T>(T value, Amazon.Lambda.Core.ILambdaSerializer serializer) {
-        using var buffer = new MemoryStream();
-        serializer.Serialize<T>(value, buffer);;
-        return System.Text.Encoding.UTF8.GetString(buffer.ToArray());
-    }
+// local functions
+string SerializeWith<T>(T value, Amazon.Lambda.Core.ILambdaSerializer serializer) {
+    using var buffer = new MemoryStream();
+    serializer.Serialize<T>(value, buffer);;
+    return System.Text.Encoding.UTF8.GetString(buffer.ToArray());
 }
 ```
 
